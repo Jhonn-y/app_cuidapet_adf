6f16fa87d2d0d775612ffda5fc0e44db6647c27a
jhonatan <jhonatanwsantosdev@gmail.com>
2024-12-18 00:22:52 -0300

    Adicionando Logger para o terminal

--- api/lib/application/config/application_config.dart
+++ api/lib/application/config/application_config.dart
@@ -1,18 +1,22 @@
 
 import 'package:cuidapet_api/application/config/database_connection_configuration.dart';
+import 'package:cuidapet_api/application/logger/i_logger.dart';
 import 'package:dotenv/dotenv.dart';
 import 'package:get_it/get_it.dart';
+
+import '../logger/logger.dart';
+
+
 
 class ApplicationConfig {
   late DotEnv env;
   Future<void> loadConfigApplication() async {
     await _loadEnv();
     _loadDatabaseConfig();
+    _configLogger();
   }
-
   Future<void> _loadEnv() async => env = DotEnv(includePlatformEnvironment: true)..load();
   
-
   void _loadDatabaseConfig() {
     final databaseConfig = DatabaseConnectionConfiguration(
         databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
@@ -22,4 +26,6 @@ class ApplicationConfig {
         password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!);
         GetIt.I.registerSingleton(databaseConfig);
   }
+  
+  void _configLogger() => GetIt.I.registerLazySingleton<ILogger>(()=> Logger());
 }
