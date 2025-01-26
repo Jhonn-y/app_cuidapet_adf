25e7d681f0d07ee578a06a6b64c3797bcdbc3f7d
jhonatan <jhonatanwsantosdev@gmail.com>
2025-01-26 13:58:25 -0300

    Implementando limpeza geral quando usuario fizer logout

--- lib/app/modules/core_module/auth/auth_store.dart
+++ lib/app/modules/core_module/auth/auth_store.dart
@@ -3,6 +3,7 @@ import 'package:mobx/mobx.dart';
 import 'package:projeto_cuidapet/app/core/helpers/constants.dart';
 import 'package:projeto_cuidapet/app/core/local_storage/local_storage.dart';
 import 'package:projeto_cuidapet/app/model/user_model.dart';
+import 'package:projeto_cuidapet/app/services/address/address_service.dart';
 
 part 'auth_store.g.dart';
 
@@ -10,11 +11,19 @@ class AuthStore = _AuthStoreBase with _$AuthStore;
 
 abstract class _AuthStoreBase with Store {
   final LocalStorage _localStorage;
+  final LocalSecureStorage _secureStorage;
+  final AddressService _addressService;
+
   @readonly
   UserModel? _userModel;
 
-  _AuthStoreBase({required LocalStorage localStorage})
-      : _localStorage = localStorage;
+  _AuthStoreBase(
+      {required LocalSecureStorage secureStorage,
+      required LocalStorage localStorage,
+      required AddressService addressService})
+      : _localStorage = localStorage,
+        _addressService = addressService,
+        _secureStorage = secureStorage;
 
   @action
   Future<void> loadUserLogged() async {
@@ -37,6 +46,8 @@ abstract class _AuthStoreBase with Store {
   @action
   Future<void> logOut() async {
     await _localStorage.clear();
+    await _secureStorage.clear();
+    await _addressService.deleteAll();
     _userModel = UserModel.empty();
   }
 }
